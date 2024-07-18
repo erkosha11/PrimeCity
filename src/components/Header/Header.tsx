cb2474b1e11886e29b50adb1415cdfa2990809a4
Erkosha <erkebulan003@yandex.ru>
2024-07-18 16:28:48 +0500

    second commit

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,18 @@
+import s from "./Header.module.scss";
+
+export const Header = () => {
+  return (
+    <header>
+      <div className="container">
+        <div className={s.content}>
+          <h1>
+            <span> Услуги</span> в Кызылорде
+          </h1>
+          <nav>
+            
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+};
