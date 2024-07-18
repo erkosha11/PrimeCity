d6f506d5e790db314a02d661123e8ea7501160f0
Erkosha <erkebulan003@yandex.ru>
2024-07-18 18:08:54 +0500

    add home

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,5 +1,27 @@
-export const Home =()=>{
-  return(
-    <div>dasd</div>
-  )
-}
+import s from "./Home.module.scss";
+import HomeImg from "../../assets/home.jpg";
+
+export const Home = () => {
+  return (
+    <section id="Home" className={s.home}>
+      <div className="container">
+        <div className={s.homeContent}>
+          <div className={s.hometxt}>
+            <h1>
+              Установочные услуги
+              <br />В Кызылорде
+            </h1>
+            <h2>Моментальный выезд!</h2>
+            <a href=""><span>+7 (701)758-72-03</span></a>
+            <p>
+              <span>90%</span> всех заявок обрабатываются и выполняются в 1 день
+            </p>
+          </div>
+          <div className={s.homeImg}>
+            <img src={HomeImg} alt="HomeImg" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
