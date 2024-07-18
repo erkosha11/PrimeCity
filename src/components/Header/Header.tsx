a702cc69b09f2f23f7684dbb06343b57612bd7a3
Erkosha <erkebulan003@yandex.ru>
2024-07-18 17:03:49 +0500

    add header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,13 +4,30 @@ export const Header = () => {
   return (
     <header>
       <div className="container">
-        <div className={s.content}>
+        <div className={s.headerContent}>
           <h1>
             <span> Услуги</span> в Кызылорде
           </h1>
           <nav>
-            
+            <ul>
+              <li>
+                <a href="">На главную</a>
+              </li>
+              <li>
+                <a href="">Услуги</a>
+              </li>
+              <li>
+                <a href="">Как мы работаем?</a>
+              </li>
+              <li>
+                <a href="">Контакты</a>
+              </li>
+            </ul>
           </nav>
+          <div className={s.headerButtons}>
+            <button>WhatsApp</button>
+            <button>Звонок</button>
+          </div>
         </div>
       </div>
     </header>
